03b1545d3e357bee1ad5b216e91fcefa225b329d
triumph <1597403461@qq.com>
2019-08-29 16:10:05 +0800

    'fkx'

---
+++ README.md
@@ -0,0 +1,16 @@
+```
+$ npm install
+```
+
+```
+$ npm run dev
+```
+
+```
+config文件夹里的config文件里的配置信息需要登录七牛云账号去获取信息
+```
+
+```
+利用postman发送文件formData对象即可。 接口是post方法：http://127.0.0.1:3001/album/upload2
+此处的127.0.0.1换成自己的本机ip地址即可。
+```
--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 const QINIU = {
-  accessKey: "KbC2l9v9USdAErpSq2SpkQdZCo64x93DLb5rvLmF",
-  secretKey: "Bd0G7WKxbdck1QG0zElYv5s3g6EH2J4YBF5L1aFH",
-  bucket: "album",
-  origin: "http://pwxg5mwk4.bkt.clouddn.com",
+  accessKey: "AK密钥",
+  secretKey: "SK密钥",
+  bucket: "存储空间名称",
+  origin: "cdn域名",
 };
 
 module.exports = QINIU
